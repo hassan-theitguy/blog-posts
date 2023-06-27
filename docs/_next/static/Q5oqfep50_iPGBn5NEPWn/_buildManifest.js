475d24b52cd3e17c00a7614a548828b8d4476594
hassan-theitguy <hassan.voyeau@gmail.com>
2023-06-27 07:40:14 -0400

    simply used import

--- docs/_next/static/Q5oqfep50_iPGBn5NEPWn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/734-b768c452f81435e1.js","static/chunks/pages/index-402412c79dea22d8.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,14 @@
-"use client"
-import useSWR from 'swr';
+import posts from "../json/blog-posts.json";
 
-interface StaticData {
-  href: string;
-  title: string | null;
-}
-
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
-const IndexPage: React.FC = () => {
-  const { data, error } = useSWR<StaticData[]>('/api/staticdata', fetcher);
-
-  if (error) {
-    return <div>Error fetching data</div>;
-  }
-
-  if (!data) {
-    return <div>Loading data...</div>;
-  }
-
+const IndexPage = () => {
   return (
-    <div>
-      <h1>All my blog posts</h1>
-      {data.map((item, index) => (
-        <div key={index}>
-          <a href={item.href}>{item.title ?? 'No title available'}</a>
-        </div>
-      ))}
-    </div>
+    <>
+      <ul>
+        {posts.map((i) => (
+          <li><a href={i.href}>{i.title ?? 'No title available'}</a></li>
+        ))}
+      </ul>
+    </>
   );
 };
 
